264a4f2a7f60d6fcdd2355fe04f658ae3a361b4e
chrislcontrol <christian.pereira@nexxera.com>
2023-05-04 02:23:18 -0300

    feat: new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='Python type enforcer',
     long_description='Python type enforcer',
     packages=find_packages(exclude=["*tests*"]),
-    version='1.0.8',
+    version='1.0.9',
     install_requires=[
         'wheel'
     ],
