b66201fbca1b0543f58c392bdcca60f7218ca7a8
chrislcontrol <christian.pereira@nexxera.com>
2023-05-04 11:08:22 -0300

    chore: doc provided

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
+import codecs
+
 from setuptools import setup, find_packages
+
+
+# -*- Long Description -*-
+
+
+def long_description():
+    try:
+        return codecs.open('README.md', 'r', 'utf-8').read()
+    except OSError:
+        return 'Long description error: Missing README.rst file'
+
 
 setup(
     name='typyd',
     description='Python type enforcer',
-    long_description='Python type enforcer',
+    long_description=long_description(),
     packages=find_packages(exclude=["*tests*"]),
-    version='1.0.10',
+    version='1.1.0',
     install_requires=[
         'wheel'
     ],
--- typyd/decorators.py
+++ typyd/decorators.py
@@ -1,11 +1,14 @@
-from typing import Callable
+from typing import Callable, Type
 
+from typyd import IValidateAnnotationsUseCase
 from typyd.use_cases.validate_annotations_use_case import ValidateAnnotationsUseCase
 
 
-def typed_function(func, ignore_float_and_integer_difference: bool = True) -> Callable:
+def typed_function(func,
+                   ignore_float_and_integer_difference: bool = True,
+                   typing_validator: Type[IValidateAnnotationsUseCase] = ValidateAnnotationsUseCase) -> Callable:
     def return_func(*args, **kwargs):
-        core = ValidateAnnotationsUseCase(func, ignore_float_and_integer_difference=ignore_float_and_integer_difference)
+        core = typing_validator(func=func, ignore_float_and_integer_difference=ignore_float_and_integer_difference)
         core.execute(*args, **kwargs)
         return func(*args, **kwargs)
 
