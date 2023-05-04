ab46eae5863b582089fd4f85e558e7f104ef8b0e
chrislcontrol <christian.pereira@nexxera.com>
2023-05-04 01:55:26 -0300

    refactor: added ValidateAnnotationsUseCase to import all

--- typyd/__init__.py
+++ typyd/__init__.py
@@ -1,9 +1,13 @@
 from typyd.contracts import IValidateAnnotationsUseCase
 from typyd.decorators import typed_function
 from typyd.typed_class import TypedClass
+from typyd.use_cases.validate_annotations_use_case import ValidateAnnotationsUseCase
+
 
 __all__ = [
     'typed_function',
     'TypedClass',
-    'IValidateAnnotationsUseCase'
+    'IValidateAnnotationsUseCase',
+    'ValidateAnnotationsUseCase'
 ]
+
